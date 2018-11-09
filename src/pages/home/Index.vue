0e7a295bfc7e202791d26cc9aa17fa3e6065d426
angel <858065385@qq.com>
2018-11-09 17:19:03 +0800

    初始化PCblog

---
+++ src/pages/home/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <span>首页</span> 
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+    }
+</script>
+ 
+<style lang="scss">
+span{
+    color: $themeColor;
+}
+</style>
